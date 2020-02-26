29317ece2966b37a13177ce99f57c3cd036fd647
Alexander.Zinchenko <az67128@gmail.com>
2020-02-26 23:30:19 +0300

    Edit component

---
+++ src/businessObjects/TroodCoreBusinessObjects/payment/editComponent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EditComponents = (props)=>{
+    console.log(props)
+    return <div>edit</div>
+
+}
+
+export default EditComponents
--- src/componentLibraries/LegalCoreComponents/Payments/index.js
+++ src/componentLibraries/LegalCoreComponents/Payments/index.js
@@ -1,7 +1,62 @@
 import React from 'react'
+import TTable from '$trood/components/TTable'
+import AsyncEntitiesList from '$trood/components/AsyncEntitiesList'
+import TIcon, { ICONS_TYPES } from '$trood/components/TIcon'
 
-const Payments = ()=>{
-    return <div>Payments</div>
+const Payments = ({paymentEntities, paymentApiActions, paymentEditorActions={}})=>{
+    console.log(paymentEditorActions.editChildEntity)
+    const paymentsApiConfig = {
+        filter: {
+          depth: 3,
+          q: '',
+        },
+      }
+    const paymentsArray = paymentEntities.getArray(paymentsApiConfig)
+    const  paymentsNextPage = paymentEntities.getNextPage(paymentsApiConfig)
+    const paymentsIsLoading = paymentEntities.getIsLoadingArray(paymentsApiConfig)
+    const paymentsNextPageAction = () => paymentApiActions.loadNextPage(paymentsApiConfig)
+    console.log(paymentsArray)
+    return <div>
+        <AsyncEntitiesList {...{
+          
+          nextPage: paymentsNextPage,
+          isLoading: paymentsIsLoading,
+          nextPageAction: paymentsNextPageAction,
+        }}>
+        <TTable {...{
+            body: paymentsArray,
+            header: [
+              {
+                title: 'payment #',
+                name: 'number',
+                sortable: true,
+                model: item => item.number || '-',
+              },
+              {
+                title: 'client',
+                name: 'client',
+                model: item => (
+                  item.name
+                ),
+              },
+              
+              {
+                title: '',
+                name: 'edit',
+                
+                model: item => (
+                  <TIcon {...{
+                    size: 18,
+                    type: ICONS_TYPES.edit,
+                    onClick: () => paymentEditorActions.editChildEntity(item),
+                  }} />
+                ),
+              },
+            ],
+            
+          }} />
+          </AsyncEntitiesList>
+    </div>
 }
 
 export default Payments
